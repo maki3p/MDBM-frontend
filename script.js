aab0fe5aabfc4d4d95851146f1aa3ceb9b869dbb
maki3p <josifovski.marjan@hotmail.com>
2017-03-22 16:01:59 +0100

    ok

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 $(document).ready(function () {
 $("#addDvd").on("click",function(){
     $("#section").append(`
-    <label class="font-color">Number of DvD <br/></label><input type="text" style="width: 35px">
-    <button type="button" class="btn btn-primary" id="addMovie">Add Movie</button`)
+    <label class="font-color">Number of DvD: &nbsp;</label><input type="text" style="width: 35px">
+    &#360;<button type="button" class="btn btn-primary" id="addMovie">Add Movie</button`)
 })
 $(document).on("click","#addMovie", function(){
     $("#section").append(`<form>
-    <label>Name of movie</label><input type="text"></input>
+    <label class="font-color" >Name of movie: <br/></label><input type="text" class="form-control">
+    <select name="sostojki" class="genre">
+        <option id="default" selected disabled="" >
+			<label for="default">Genre</label>
+		 </option>>
+		<option value="thriller" >Thriller</option>
+		<option value="action">Action</option>
+		<option value="horror">Horror</option>
+        <option value="comedy">Comedy</option>
+        <option value="romacne">Romacne</option>
+        <option value="drama">Drama</option>
+        <option value="musical">Musical</option>
+        <option value="western">Western</option>
+        <option value="adventure">Adventure</option>
+        <option value="animation">Animation</option>
+        <option value="scifi">Sci-Fi</option>          
+	</select><br/>
+    <label class="font-color" >Cast : <br/></label>  <input type="text">
     </form>`)
 })
 
