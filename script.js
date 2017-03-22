a2b90022b93e451916699036dbba9a3847b1dc23
maki3p <josifovski.marjan@hotmail.com>
2017-03-22 15:15:11 +0100

    a

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+$(document).ready(function () {
+$("#addDvd").on("click",function(){
+    $("#section").append(`
+    <label class="font-color">Number of DvD <br/></label><input type="text" style="width: 35px">
+    <button type="button" class="btn btn-primary" id="addMovie">Add Movie</button`)
+})
+$(document).on("click","#addMovie", function(){
+    $("#section").append(`<form>
+    <label>Name of movie</label><input type="text"></input>
+    </form>`)
+})
 
-
+})
