b9e24f76cf63b47961f4f1845ee0a1e71a12caa5
maki3p <josifovski.marjan@hotmail.com>
2017-06-07 11:10:48 +0200

    new

--- script.js
+++ script.js
@@ -160,15 +160,7 @@ $(document).ready(function () {
     }
   });
 
-  $("#search").keyup(function () {
-    var value = this.value;
-
-    $("table").find("tr").each(function (index) {
-      if (!index) return;
-      var id = $(this).find("td").first().text();
-      $(this).toggle(id.indexOf(value) !== -1);
-    });
-  });
+ 
   
 
 });
